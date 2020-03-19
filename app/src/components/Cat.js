a92e4ded8bb17e8af375265cf8291e494ad1b37c
Cody Denniston <dennistonc4817@gapps.bcsds.org>
2020-03-19 08:43:15 -0500

    just have to style

--- app/src/components/Cat.js
+++ app/src/components/Cat.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchCat } from '../actions';
+import Loader from 'react-loader-spinner';
 
-const Cat = () => {
+const Cat = props => {
 	return (
 		<div>
-			<h2>KITTYS</h2>
+			<button onClick={props.fetchCat}>Get Kitty</button>
+			{!props.cat && !props.isLoading && <h2>Get a new kitty!</h2>}
+			{props.isLoading && (
+				<Loader
+					type='Puff'
+					color='#00BFFF'
+					height={100}
+					width={100}
+					timeout={3000} //3 secs
+				/>
+			)}
+			{props.cat && !props.isLoading && <img src={props.cat.file} />}
 		</div>
 	);
 };
-export default Cat;
+const mapStateToProps = state => {
+	return {
+		isLoading: state.isLoading,
+		cat: state.cat,
+		error: state.error
+	};
+};
+
+export default connect(mapStateToProps, { fetchCat })(Cat);
--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -1,11 +1,22 @@
 const initialState = {
 	isLoading: false,
-	image: {},
+	cat: null,
 	error: ''
 };
 
 export const reducer = (state = initialState, action) => {
 	switch (action.type) {
+		case 'FETCHING_CAT_START':
+			return {
+				...state,
+				isLoading: true
+			};
+		case 'FETCHING_CAT_SUCCESS':
+			return {
+				...state,
+				isLoading: false,
+				cat: action.payload
+			};
 		default:
 			return state;
 	}
