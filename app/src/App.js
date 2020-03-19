9b447fbeaf5522e637546d477a5771a5e6f63ce8
Cody Denniston <dennistonc4817@gapps.bcsds.org>
2020-03-19 07:40:57 -0500

    got some structure set up

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Cat from './components/Cat';
+
+function App() {
+	return (
+		<div className='App'>
+			<Cat />
+		</div>
+	);
+}
+
+export default App;
---
+++ app/src/components/Cat.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Cat = () => {
+	return (
+		<div>
+			<h2>KITTYS</h2>
+		</div>
+	);
+};
+export default Cat;
